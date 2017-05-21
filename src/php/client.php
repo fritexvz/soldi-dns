982612152a035b281183abbe05294e7ee698aab3
Fritex <fritexvz@users.noreply.github.com>
2017-05-22 00:50:55 +0200

    Add files via upload

---
+++ src/php/client.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Project Name: Soldi-dns
+ * Project URI: https://github.com/fritexvz/soldi-dns/
+ * Author: Tomislav 'Fritex' Novak
+ * Author URI: http://www.tomislavnovak.com.hr/
+ * Description: Re-decentralizing the DNS.hr with digital identity .from.hr.
+ * Version: 1.0
+ * License: GNU General Public License v3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Tags: varazdin, varaždin, croatia, hrvatska, dns, .from.hr, .hr, 2017
+ *
+ */
+
+?>
